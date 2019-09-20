70e67be7c65545e4e635a5fd3150e20aac807ac1
talithavanlith <43021762+talithavanlith@users.noreply.github.com>
2019-09-20 03:03:01 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>~~ This website was created for a University project. The content is largely fictional and no services are actually being offered ~~
-    </p>
-</footer>
-</body>
-</html>
